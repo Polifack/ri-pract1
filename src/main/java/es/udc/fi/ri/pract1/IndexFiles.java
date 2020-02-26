39856537df9d29db46d0f7672586a53e4cfa9ccb
Poli <diegoroca.ordes@gmail.com>
2020-02-26 10:59:24 +0100

    Añadidos fields lucene

--- src/main/java/es/udc/fi/ri/pract1/IndexFiles.java
+++ src/main/java/es/udc/fi/ri/pract1/IndexFiles.java
@@ -1,9 +1,13 @@
 package es.udc.fi.ri.pract1;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -12,10 +16,13 @@ import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.document.DoublePoint;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.LongPoint;
 import org.apache.lucene.document.StringField;
@@ -27,10 +34,43 @@ import org.apache.lucene.index.Term;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
-
 public class IndexFiles {
 
 	private IndexFiles() {
+	}
+
+	private static String readFile(String fileName) throws IOException{
+		String line = null;
+		String result = "";
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+
+		int i = 0;
+			while (((line = bufferedReader.readLine()) != null) && i < 5) {
+				result +=line+"\n";
+				i++;
+			}
+		bufferedReader.close();
+		return result;
+	}
+	
+	private static String readFileBack(String fileName) throws IOException{
+		String result = "";
+        BufferedReader br = new BufferedReader(
+        		new InputStreamReader(new FileInputStream(fileName)));
+		List<String> lines = new LinkedList<String>();
+		
+		//Eliminamos la primera linea de la lista hasta que haya solo 5 lineas
+		for(String tmp; (tmp = br.readLine()) != null;) 
+		    if (lines.add(tmp) && lines.size() > 5) 
+		        lines.remove(0);
+		
+		for (String line : lines) {
+			result += line+"\n";
+		}
+		
+		br.close();
+		return result;
+		
 	}
 
 	static void indexDocs(final IndexWriter writer, Path path) throws IOException {
@@ -53,14 +93,23 @@ public class IndexFiles {
 	static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
 		try (InputStream stream = Files.newInputStream(file)) {
 			Document doc = new Document();
-
-			Field pathField = new StringField("path", file.toString(), Field.Store.YES);
-			doc.add(pathField);
-
+			
+			//Path del archivo
+			doc.add(new StringField("path", file.toString(), Field.Store.YES));
+			//Fecha de modificacion
 			doc.add(new LongPoint("modified", lastModified));
-
-			doc.add(new TextField("contents",
-					new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+			//Contenidos totales del archivo
+			doc.add(new TextField("contents",new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+			//Hostname del indexer
+			doc.add(new StringField("hostname", InetAddress.getLocalHost().getHostName(), Field.Store.YES));
+			//Thread que indexa
+			doc.add(new StringField("thread", Thread.currentThread().getName(), Field.Store.YES));
+			//Tamaño en kbs
+			doc.add(new DoublePoint("sizeKb", (double) (new File(file.toString()).length() / 1024)));
+			//Primeras 5 lineas
+			doc.add(new TextField("top5Lines", readFile(file.toString()), Field.Store.YES)) ;
+			//Ultimas 5 lineas
+			doc.add(new TextField("bottom5Lines", readFileBack(file.toString()), Field.Store.YES)) ;
 
 			if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
 				System.out.println("adding " + file);
